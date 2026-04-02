93aa5981b5cd34c7e1e5a3c0d3d1e9e78bcb80fd
Dinesh Kumar Baghel <149395660+DineshKumarBaghel07@users.noreply.github.com>
2026-04-02 06:57:24 +0530

    Update CORS origin for frontend URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"https://healthcarepluse-frontend.onrender.com",
+    origin:"https://healthcarepluse-2.onrender.com/",
     credentials: true,
   }),
 );
