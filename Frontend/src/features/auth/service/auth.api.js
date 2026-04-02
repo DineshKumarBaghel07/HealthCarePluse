503fac41093001176d1aef292ce714e6d14c322e
Dinesh Kumar Baghel <149395660+DineshKumarBaghel07@users.noreply.github.com>
2026-04-02 07:20:17 +0530

    Remove trailing slash from API URL

--- Frontend/src/features/auth/service/auth.api.js
+++ Frontend/src/features/auth/service/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = import.meta.env.VITE_API_URL || "https://healthcarepluse.onrender.com/";
+const url = import.meta.env.VITE_API_URL || "https://healthcarepluse.onrender.com";
 const api = axios.create({
   baseURL: url,
   withCredentials: true,
